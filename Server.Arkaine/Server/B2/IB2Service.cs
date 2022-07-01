138f81fe2f5a60c657b27d243552ca94ee343ef8
Rob Davis <bfe5e925@opayq.com>
2022-07-01 17:33:30 +1000

    Adding missed file

--- Server.Arkaine/Server/B2/IB2Service.cs
+++ Server.Arkaine/Server/B2/IB2Service.cs
@@ -2,7 +2,7 @@
 {
     public interface IB2Service
     {
-        Task<AuthResponse> GetToken(string keyId, string key, CancellationToken cancellationToken);
+        Task<AuthResponse> GetToken(string key, CancellationToken cancellationToken);
         Task<AlbumsResponse> ListAlbums(string userName, CancellationToken cancellationToken);
         Task<FilesResponse> ListFiles(FilesRequest request, string userName, CancellationToken cancellationToken);
         Task<IResult> Stream(string userName, string bucketName, string fileName, CancellationToken cancellationToken);
