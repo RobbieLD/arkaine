e49cd5619ae680664208a507d4049dd7fda23900
Rob Davis <bfe5e925@opayq.com>
2022-07-04 16:57:21 +1000

    Added config to chrome ext

---
+++ Chrome.Arkaine/scripts/config.js
@@ -0,0 +1,26 @@
+function save_options() {
+    var key = document.getElementById('api-key').value;
+    var url = document.getElementById('api-url').value;
+    chrome.storage.sync.set({ key, url }, () => {
+        // Update status to let user know options were saved.
+        var status = document.getElementsByClassName('status')[0];
+        status.textContent = 'Options saved.';
+        setTimeout(() => {
+            status.textContent = '';
+        }, 750);
+    });
+}
+
+function restore_options() {
+    chrome.storage.sync.get({
+        key: '',
+        url: ''
+    }, (items) => {
+        document.getElementById('api-key').value = items.key;
+        document.getElementById('api-key').value = items.url;
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
--- Chrome.Arkaine/scripts/processor.js
+++ Chrome.Arkaine/scripts/processor.js
@@ -5,9 +5,19 @@ const log = (message) => {
     console.appendChild(entry)
 }
 
+const process = (url, key, api) => {
+    console.log(url, key, api)
+}
+
 log('Starting...')
 
 chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    // Get the Tab url we're going to use
     let url = tabs[0].url;
     log(url)
-});
+
+    // Get the api key
+    chrome.storage.sync.get({ key: '', url: '' }, (items) => {
+        process(url, items.key, items.url)
+    })
+})
